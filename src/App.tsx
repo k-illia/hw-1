b886555471f0860f27c3953d7be556ed5feed2e0
Илья <illia.kurishko@gmail.com>
2023-07-17 17:58:03 +0300

    task 3

--- src/App.tsx
+++ src/App.tsx
@@ -7,9 +7,11 @@ import { HW4 } from './HW/4/HW4';
 function App() {
   return (
     <div className="App">
+
+
       <HW1 />
       <HW2 />
-      {/*<HW3 />*/}
+      <HW3 />
       {/*<HW4 />*/}
     </div>
   );
